fa5722c4599102d007207f3f112a0b3b65daee8c
Tania Berezovski <cocakolata@gmail.com>
2014-09-19 10:38:50 +0300

    Initial Commit

---
+++ CigarettesAndMoneyCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CigarettesAndMoneyCounter
+//
+//  Created by Tania on 9/19/14.
+//  Copyright (c) 2014 Tania Berezovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
