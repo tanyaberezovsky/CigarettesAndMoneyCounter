fda01b30bbfc1fbf532bf4c72bcd64ee9d9410f2
Tania Berezovski <cocakolata@gmail.com>
2015-07-18 16:02:26 +0300

    18-7

--- CigarettesAndMoneyCounter/CalculatorModal.swift
+++ CigarettesAndMoneyCounter/CalculatorModal.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+
+//model
+class Calculator{
+    
+var dailyCiggarets:Double = 1
+//var packCost:Double = 1
+
+    //Key-Value-Observing (KVO) define Property Observers
+    //Property Observers are not called during Initialization
+    func setValue(ciggarets: Double, segment: Int)
+    {
+        self.dailyCiggarets = ciggarets / segmentToDays(segment)
+        // call recalculate function
+        // fireEvent to update GUI
+    }
+    
+    var packCost: Double = 0.0{
+        willSet
+        {
+            println("About to set status to:  \(newValue)")
+        }
+        didSet
+        {
+            if packCost != oldValue
+            {
+         //       postNewStatusNotification()
+            }
+        }
+    }
+    /*
+    func setValue(packCost: Double)
+    {
+        self.packCost = packCost
+        // call recalculate function
+        // fireEvent to update GUI
+    }
+    */
+    func segmentToDays(segment: Int) -> Double
+    {
+        var ret: Double = 1
+        
+        switch segment{
+        case 1:
+            ret = 7
+        case 2:
+            ret = 30
+        case 3:
+            ret = 360
+        default:
+            break
+        }
+        return ret
+    }
+
+    func getValues(segment: Int)-> (Double, Double)
+    {
+    
+        return (dailyCiggarets * segmentToDays(segment), packCost * segmentToDays(segment))
+    }
+    
+}
