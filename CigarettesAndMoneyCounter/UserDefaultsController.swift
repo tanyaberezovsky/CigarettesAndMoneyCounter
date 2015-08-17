732a5e6aec869904236257da65958893a68e8740
Tania Berezovski <cocakolata@gmail.com>
2015-08-17 23:36:53 +0300

    finish calculator window

--- CigarettesAndMoneyCounter/UserDefaultsController.swift
+++ CigarettesAndMoneyCounter/UserDefaultsController.swift
@@ -20,12 +20,17 @@ class UserDefaultsController: UIViewController,TableLevelsControllerDelegate {
     
     @IBOutlet var dailyGoal: UITextField!
     
-    @IBOutlet var buttonSave: UIButton!
     
     var myDelegate:UserDefaultsControllerDelegate? = nil
     
+    override func didMoveToParentViewController(parent: UIViewController?) {
+        if parent == nil {
+            SaveDafaultsTouch()
+        }
+    }
+
     
-    @IBAction func SaveDafaultsTouch(sender: UIButton) {
+    func SaveDafaultsTouch() {
         var defaults = UserDefaultsDataController()
         var userDefaults = UserDefaults()
         
@@ -49,7 +54,7 @@ class UserDefaultsController: UIViewController,TableLevelsControllerDelegate {
         if(myDelegate != nil){
             myDelegate!.ReloadUserDefaults()
         }
-        
+      
     }
     
     func barButtonItemClicked(){
@@ -66,7 +71,7 @@ class UserDefaultsController: UIViewController,TableLevelsControllerDelegate {
     
     //init variable and set segueid into it
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if  segue.identifier == "segueLvlOfNeededSettings" || segue.identifier == "segueLvlOfEnjoySettings"{
+        if  segue.identifier == segueNames.segueLvlOfNeeded || segue.identifier == segueNames.segueLvlOfEnjoy{
             let vc = segue.destinationViewController as! TableLavels
             vc.segueSourceName = segue.identifier
             vc.myDelegate = self
@@ -88,9 +93,6 @@ class UserDefaultsController: UIViewController,TableLevelsControllerDelegate {
     //  load Graphics Settings
     //++++++++++++++++++++++++++++++++++++
     func loadGraphicsSettings() {
-        //round button conners
-        var btnLayer: CALayer = buttonSave.layer
-        btnLayer.cornerRadius = 10
         
         //set button look like text field
         var layerLevelAsNeeded: CALayer = levelAsNeeded.layer
@@ -133,12 +135,12 @@ class UserDefaultsController: UIViewController,TableLevelsControllerDelegate {
     received selected row value from TableLevels and set it to apropriate field
     */
     func myColumnDidSelected(controller: TableLavels, text: String, segueName: String) {
-        if segueName == "segueLvlOfEnjoySettings"{
+        if segueName == segueNames.segueLvlOfEnjoy{
             levelOfEnjoyText = text;
             println(text)
             levelOfEnjoy.setTitle(levelOfEnjoyText, forState: UIControlState.Normal)
         }
-        if segueName == "segueLvlOfNeededSettings"{
+        if segueName == segueNames.segueLvlOfNeeded{
             levelAsNeededText = text;
             println(text)
             levelAsNeeded.setTitle(levelAsNeededText, forState: UIControlState.Normal)
