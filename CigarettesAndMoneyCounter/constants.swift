b65abd52301eff546093deb313ade17e902ee620
Tania Berezovski <cocakolata@gmail.com>
2015-08-28 17:04:51 +0300

    2015-08-28 add gradient navigation bar

---
+++ CigarettesAndMoneyCounter/constants.swift
@@ -0,0 +1,15 @@
+//
+//  constants.swift
+//  CigarettesAndMoneyCounter
+//
+//  Created by Tania on 8/28/15.
+//  Copyright (c) 2015 Tania Berezovski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let colorNavigationBarTop = RGB(41, 128, 185, alpha: 1)
+let colorNavigationBarBottom = RGB(34, 160, 145, alpha: 1)
+
+let colorNavigationBarBlack = RGB(24, 25, 35, alpha: 1)
