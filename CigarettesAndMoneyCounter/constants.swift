a27d84f7cc6fa8479405ef11e1e81d0afd5891ec
Tania Berezovski <cocakolata@gmail.com>
2015-08-30 22:33:46 +0300

    2015-08-30

--- CigarettesAndMoneyCounter/constants.swift
+++ CigarettesAndMoneyCounter/constants.swift
@@ -13,3 +13,7 @@ let colorNavigationBarTop = RGB(41, 128, 185, alpha: 1)
 let colorNavigationBarBottom = RGB(34, 160, 145, alpha: 1)
 
 let colorNavigationBarBlack = RGB(24, 25, 35, alpha: 1)
+
+let colorSegmentTint = RGB(239, 239, 244, alpha: 1)
+
+let colorSegmentBackground = RGB(0 , 184, 156, alpha: 1)
