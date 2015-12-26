4c6cc54d511a33862a7e10dce05192994ee3bee4
Tania Berezovski <cocakolata@gmail.com>
2015-12-26 17:58:30 +0200

    2015-12-26

--- CigarettesAndMoneyCounter/SummaryViewController.swift
+++ CigarettesAndMoneyCounter/SummaryViewController.swift
@@ -9,6 +9,7 @@
 
 import UIKit
 import CoreData
+import Charts
 
 class SummaryViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,NSFetchedResultsControllerDelegate
 {
@@ -23,6 +24,11 @@ class SummaryViewController: UIViewController,UIPickerViewDataSource,UIPickerVie
     @IBOutlet weak var smoked: UILabel!
     @IBOutlet weak var cost: UILabel!
     
+    @IBOutlet weak var pieChartView: PieChartView!
+  //  @IBOutlet weak var barChartView: BarChartView!
+
+ //   @IBOutlet weak var pieChartView: PieChartView!
+   
     @IBOutlet weak var segmentDateType: UISegmentedControl!
     @IBOutlet weak var selectedDate: UITextField!
     
@@ -46,7 +52,7 @@ class SummaryViewController: UIViewController,UIPickerViewDataSource,UIPickerVie
 
         pickerData.append(arrYear)
     }
-    //80B53D53B6
+    //password azriely 80B53D53B6
     @IBAction func shiftBtnDateSubOnTouch(sender: UIButton) {
         calculateSelectedDate(-1)
     }
@@ -61,10 +67,65 @@ class SummaryViewController: UIViewController,UIPickerViewDataSource,UIPickerVie
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pieChartView.noDataTextDescription = "Have five"
+        pieChartView.noDataText = "congratulation, you don't smoke at this period"
+    
         createYearArr()
         loadScreenGraphics();
 
     }
+    
+    func setChartPie(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Units Sold")
+        
+        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for i in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+        
+        
+//        let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "Units Sold")
+//        let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+//        lineChartView.data = lineChartData
+//        
+    }
+
+   /* func setChart(dataPoints: [String], values: [Double]) {
+        
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(xVals: dataPoints, dataSet: chartDataSet)
+        barChartView.data = chartData
+        
+    }*/
     
     enum pickerComponent:Int{
         case size = 0
@@ -184,6 +245,32 @@ class SummaryViewController: UIViewController,UIPickerViewDataSource,UIPickerVie
         
         smoked.text  = String(smokeAndCost.smoked)
         cost.text = decimalFormatToString(smokeAndCost.cost)
+        
+        let fieldName = "reason"
+        let arrReason:NSArray = cigRecord.calculateGraphDataByFieldName(curentDate, toDate: toDate, fieldName: fieldName)
+        
+        if arrReason.count > 0 {
+        
+            var description = [String]()
+            var sumOfCigs = [Double]()
+            
+            for var i = 0; i < arrReason.count; i++
+            {
+                if let desc:String = (arrReason[i] as! NSDictionary)[fieldName] as? String {
+                    description.append(desc)
+                }
+                if let cigs = (arrReason[i] as! NSDictionary)["sumOftotalCigarettes"] as? NSNumber {
+                    sumOfCigs.append(Double(cigs))
+                }
+            }
+            
+//            let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+//            let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+//            setChartPie(months, values: unitsSold)
+    
+         setChartPie(description, values: sumOfCigs)
+        }
+
     }
     
     func getStringDate(dDate: NSDate, currentDateFormat: String)->String
@@ -197,6 +284,7 @@ class SummaryViewController: UIViewController,UIPickerViewDataSource,UIPickerVie
     
     func loadScreenGraphics()
     {
+        
         myPicker.delegate = self
         myPicker.hidden = true
         
@@ -210,7 +298,10 @@ class SummaryViewController: UIViewController,UIPickerViewDataSource,UIPickerVie
     //    showSelectedDate(NSDate(), dateFormat: Constants.dateFormat.day)
         
         calculateSelectedDate(0)
+        
+        
     }
+    
     
     //set selected date to text field
     func handleDatePicker(sender: UIDatePicker) {
